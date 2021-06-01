1d63d224fe60913f97bea8ddca6f2744917c3e37
Monish-R45 <81694620+Monish-R45@users.noreply.github.com>
2021-06-01 16:10:56 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -16,9 +16,10 @@ class Stone{
 	}
 	display()
 	{
-			var stonepos=this.body.position;		
+			var stonepos=this.body.position;
+		        var angle = this.body.angle
 			push()
-			translate(stonepos.x, stonepos.y);
+			translate(stonepos.x, stonepos.y
 		        rotate(angle);
 			rectMode(CENTER)
 			strokeWeight(4);
